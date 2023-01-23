bc8dbdfe224ce55f773486aab7e3db1c4a25574e
kawasima <kawasima1016@gmail.com>
2023-01-23 16:54:09 +0900

    コメントを追加

--- src/victim_purity.ts
+++ src/victim_purity.ts
@@ -8,6 +8,14 @@ import {
  } from './share'
 
 // 性能を犠牲にせず、かつドメイン層にあるべき責務をドメインレイヤで実装する
+
+/**
+ * 更新するときにカートに不変条件のチェックを持たせたい。
+ * ↓
+ * が、そのためにカートアイテムを全部インスタンス化して持たせることは性能観点からやりたくない
+ * ↓
+ * ので、更新用と参照用を分けて、更新時の不変条件チェックを全アイテムロードすることなく実施する
+ */
 class CartForUpdate {
     private static UPPER_BOUND: number = 10000;
     private owner: UserId;
@@ -20,6 +28,10 @@ class CartForUpdate {
         if (total + quantity > CartForUpdate.UPPER_BOUND) {
             throw new Error(`商品数の上限に達しています`)
         }
+        cartRepository.addItem({
+            productId,
+            quantity
+        });
     }
 }
 
@@ -33,6 +45,7 @@ interface CartWriteRepository {
     loadCart(userId: UserId): CartForUpdate;
     saveCart(cart: CartForUpdate): void;
     getItemCount(): number;
+    addItem(cart: CartItem): void;
 }
 
 interface CartReadRepository {
