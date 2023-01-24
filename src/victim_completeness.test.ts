3626fe8ea227997d636991f1bda0b594f3a58bb5
kawasima <kawasima1016@gmail.com>
2023-01-24 15:03:00 +0900

    Add test cases

--- src/victim_completeness.test.ts
+++ src/victim_completeness.test.ts
@@ -23,3 +23,27 @@ test('add an item successfully', () => {
         quantity: 8,
     })
 })
+
+test('cart full', () => {
+    const cartRepository: CartRepository = {
+        loadCart: userId => new Cart(randomUUID()),
+        saveCart: cart => {},
+        getItemCount: cartId => 10000,
+        addItem: (productId, quantity) => {
+            fail()
+        }
+    }
+    const productRepository: ProductRepository = {
+        isNowOnSale: productId => true,
+    }
+    expect(() => addItemToCartUseCase(
+            cartRepository,
+            productRepository
+        )({
+            userId: randomUUID(),
+            productId: randomUUID(),
+            quantity: 1,
+        })
+    ).toThrow(/上限/)
+
+})
--- src/victim_purity.test.ts
+++ src/victim_purity.test.ts
@@ -24,3 +24,27 @@ test('add an item', () => {
         quantity: 8,
     })
 })
+
+test('cart full', () => {
+    const cartRepository: CartWriteRepository = {
+        loadCart: (userId) => new CartForUpdate(randomUUID()),
+        saveCart: (cart) => {
+            expect(cart)
+        },
+        getItemCount: () => 10000,
+        addItem: (cartItem) => {
+        },
+    }
+    const productRepository: ProductRepository = {
+        isNowOnSale: productId => true,
+    }
+    expect(() => addItemToCartUseCase(
+            cartRepository,
+            productRepository
+        )({
+            userId: randomUUID(),
+            productId: randomUUID(),
+            quantity: 1,
+        })
+    ).toThrow(/上限/)
+})
